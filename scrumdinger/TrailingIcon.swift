a0759680b38da9f5f95e7dd291fbdd774a9be5b7
Fernando Crozetta <fernando@czetta.com>
2024-06-08 00:37:59 +0200

    end of module
    ready to display in other view

--- scrumdinger/TrailingIcon.swift
+++ scrumdinger/TrailingIcon.swift
@@ -5,4 +5,20 @@
 //  Created by Fernando Crozetta on 08/06/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+    
+    
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self {Self()}
+}
