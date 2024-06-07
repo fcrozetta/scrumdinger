98259af7c1c099a0c2d19223df64dbdbaa8754d4
Fernando Crozetta <fernando@czetta.com>
2024-06-07 22:56:29 +0200

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Fernando Crozetta on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
