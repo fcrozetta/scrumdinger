0cc4abbb0d8dcf0dc1a8e4eb7438299dc2478e20
Fernando Crozetta <fernando@czetta.com>
2024-06-08 15:58:41 +0200

    added details in view

--- scrumdinger/ScrumsView.swift
+++ scrumdinger/ScrumsView.swift
@@ -10,9 +10,21 @@ import SwiftUI
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
-        List(scrums,id: \.title) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                        
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar{
+                Button(action: {} ) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("new scrum")
+            }
         }
     }
 }
